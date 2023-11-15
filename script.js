c64fcf62e65af59da8bccd93f1ed622dad097965
Jake Fontaine <72557178+JakeFontaine821@users.noreply.github.com>
2023-11-15 17:08:07 -0500

    added some on/off screen animations

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ const body = document.querySelector("body"),
       login_button = body.querySelector(".login-button"),
       back_buttons = body.querySelectorAll(".back-button");
 
+login_menu.style.visibility='hidden';
+
 login_button.addEventListener("click", () => {
     landing_menu.classList.toggle("off-screen");
+
     login_menu.classList.toggle("off-screen");
+    login_menu.style.visibility='visible';
 })
 
 for (let i = 0; i < back_buttons.length; i++) {
