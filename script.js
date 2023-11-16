55296e53f14eda13db6acbda81e4e2a2e9b31abe
Jake Fontaine <72557178+JakeFontaine821@users.noreply.github.com>
2023-11-15 22:53:38 -0500

    Last push for tonight
    Added Sign up page and reworked javascript to work with all pages

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
 const body = document.querySelector("body"),
       landing_menu = body.querySelector("main"),
-      login_menu = body.querySelector(".login-page"),
       login_button = body.querySelector(".login-button"),
+      signup_button = body.querySelector(".signup-button"),
+      login_menu = body.querySelector(".login-page"),      
+      signup_menu = body.querySelector(".signup-page"),
       back_buttons = body.querySelectorAll(".back-button");
 
-login_menu.style.visibility='hidden';
+const Pages = {
+    Landing: 0,
+    Login: 1,
+    Signup: 2
+}
+let current_page = Pages.Landing;
+
+function SwitchPages(_nextPage) {
+    switch (current_page) {
+        case Pages.Landing:
+            landing_menu.classList.toggle("off-screen");
+            break;
+        case Pages.Login:
+            login_menu.classList.toggle("off-screen");
+            break;
+        case Pages.Signup:
+            signup_menu.classList.toggle("off-screen");
+            break;
+    }
+
+    switch (_nextPage) {
+        case Pages.Landing:
+            landing_menu.classList.toggle("off-screen");
+            break;
+        case Pages.Login:
+            login_menu.classList.toggle("off-screen");
+            break;
+        case Pages.Signup:
+            signup_menu.classList.toggle("off-screen");
+            break;
+    }
+
+    current_page = _nextPage;
+}
 
 login_button.addEventListener("click", () => {
-    landing_menu.classList.toggle("off-screen");
+    SwitchPages(Pages.Login);
+})
 
-    login_menu.classList.toggle("off-screen");
-    login_menu.style.visibility='visible';
+signup_button.addEventListener("click", () => {
+    SwitchPages(Pages.Signup);
 })
 
 for (let i = 0; i < back_buttons.length; i++) {
     back_buttons[i].addEventListener("click", () => {
-        landing_menu.classList.toggle("off-screen");
-        login_menu.classList.toggle("off-screen");
+        SwitchPages(Pages.Landing);
     })   
 }
+
+login_menu.style.visibility='hidden';
+signup_menu.style.visibility='hidden';
+setTimeout(() => {
+    login_menu.style.visibility='visible';
+    signup_menu.style.visibility='visible';
+}, 800)
