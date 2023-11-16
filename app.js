79f6f4510c042a4467d49938c2df2734b7f755f1
Jake Fontaine <72557178+JakeFontaine821@users.noreply.github.com>
2023-11-16 17:38:48 -0500

    added database connection

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var port = process.env.port||3000;
+var mongoose = require("mongoose");
+var db = require("./config/database");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
 
-//example routes
+mongoose.connect(db.mongoURI,{
+    useNewURLParser:true
+}).then(function(){
+    console.log("Connected to MongoDB Database");
+}).catch(function(err){
+    console.log(err);
+});
+
+// Load in Schema
+require("./modules/loginSchema");
+var IssueLog = mongoose.model("logins");
+
+// Routes
 app.get("/", function(req, res){
     res.redirect("index.html");
 })
